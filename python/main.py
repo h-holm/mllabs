69998ad24c482d948e8c5330133322d5ad415d26
Henrik Holm <henholm@kth.se>
2019-09-11 15:56:34 +0200

    Still doing assignment 5

--- python/main.py
+++ python/main.py
@@ -1,5 +1,5 @@
 import monkdata as m
-from dtree import *
+import dtree as d
 from drawtree_qt5 import *
 
 
@@ -25,9 +25,9 @@ from drawtree_qt5 import *
 
 print("\n\nAssignment 1 - Calculate the entropy of each training dataset")
 
-entropy_monk1 = entropy(m.monk1)    # Yields 1.0 because there's a 50/50 split.
-entropy_monk2 = entropy(m.monk2)    # Yields 0.9571....
-entropy_monk3 = entropy(m.monk3)    # yields 0.9998....
+entropy_monk1 = d.entropy(m.monk1)    # Yields 1.0 because there's a 50/50 split.
+entropy_monk2 = d.entropy(m.monk2)    # Yields 0.9571....
+entropy_monk3 = d.entropy(m.monk3)    # yields 0.9998....
 print()
 print("monk1: ", entropy_monk1)
 print("monk2: ", entropy_monk2)
@@ -54,7 +54,7 @@ print("monk3: ", entropy_monk3)
 
 
 ################################# Assignment 3 #################################
-# Use the function averageGain (defined in dtree.py) to calculate the expected
+# Use the function d.averageGain (defined in dtree.py) to calculate the expected
 # information gain corresponding to each of the six attributes. Note that the
 # attributes are represented as instances of the class Attribute (defined in
 # monkdata.py) which you can access via m.attributes[0], ..., m.attributes[5].
@@ -67,7 +67,7 @@ def get_avg_gain_dict(dataset):
     for i in range(len(m.attributes)):
         attribute_key = "A" + str(i+1)
         attribute = m.attributes[i]
-        avg_gain = averageGain(dataset, attribute)
+        avg_gain = d.averageGain(dataset, attribute)
         avg_gain_dict[attribute_key] = avg_gain
     return avg_gain_dict
 
@@ -85,6 +85,7 @@ print("\n\nAssignment 3 - Where do we find the highest average gain?\n")
 # print(avg_gain_dict)
 [print(name, ": ", max(monk_dict.items(), key=lambda item:item[1])) for name, monk_dict in avg_gain_dict.items()]
 print()
+# print(avg_gain_dict["monk1"])
 
 # Answer:   A5 has the highest average gain in the monk1 dataset:
 #           0.28703074971578435
@@ -128,8 +129,8 @@ print()
 
 
 ################################# Assignment 5 #################################
-# Split the monk1 data into subsets according to the selected attribute using
-# the function select (again, defined in dtree.py) and compute the information
+# Split the monk1 data into subsets according to the d.selected attribute using
+# the function d.select (again, defined in dtree.py) and compute the information
 # gains for the nodes on the next level of the tree. Which attributes should be
 # tested for these nodes?
 #       For the monk1 data draw the decision tree up to the first two levels
@@ -148,61 +149,41 @@ def get_avg_gain_dict_exclude(dataset, exclude=[]):
         if i not in exclude:
             attribute_key = "A" + str(i+1)
             attribute = m.attributes[i]
-            avg_gain = averageGain(dataset, attribute)
+            avg_gain = d.averageGain(dataset, attribute)
             avg_gain_dict[attribute_key] = avg_gain
 
     return avg_gain_dict
 
 
-print("\nAssignment 5.1 a) - Split monk1 into subsets according to selected attribute {}\n".format("A5"))
-monk1_A5 = list()
-monk1_A12346 = list()
-for i in range(len(m.attributes)):
-    if i == 4:
-        monk1_A5.extend(select(m.monk1, m.attributes[i], True))
-    else:
-        monk1_A12346.extend(select(m.monk1, m.attributes[i], True))
+d.selected_attribute = "A5"
+print("\nAssignment 5.1 a) - Split monk1 into subsets according to d.selected attribute {}\n".format(d.selected_attribute))
+idx = int(d.selected_attribute[-1]) - 1
+subset_A5_true = d.select(m.monk1, m.attributes[idx], True)
+subset_A12346 = [x for x in m.monk1 if x not in subset_A5_true]
 
-# print(len(monk1_A5))
-# print(len(monk1_A12346))
+print("\nAssignment 5.1 b) - Where do we find the highest average gain?")
+IG_dict_A12346 = get_avg_gain_dict_exclude(subset_A12346, exclude=[idx])
+IG_dict_A12346 = sorted(IG_dict_A12346.items(), key=lambda kv: kv[1], reverse=True)
+# print("\n", IG_dict_A12346)
+[print(tuple[0], ":    ", tuple[1]) for tuple in IG_dict_A12346]
+print()
 
-# d1 = get_avg_gain_dict_exclude(monk1_A5)
-d2 = get_avg_gain_dict_exclude(monk1_A12346, exclude=[4])
-print("Subset of monk1 with A5 == True excluded")
-for key, value in d2.items():
-    print(key, "   : ", value)
+d.selected_attribute = "A1"
+print("\nAssignment 5.1 c) - Split into further subsets according to d.selected attribute {}\n".format(d.selected_attribute))
+old_idx = idx
+idx = int(d.selected_attribute[-1]) - 1
+subset_A1_true = d.select(subset_A12346, m.attributes[idx], True)
+subset_A2346 = [x for x in subset_A12346 if x not in subset_A1_true]
+
+print(d.buildTree(m.monk1, m.attributes, 3))
+drawTree(d.buildTree(m.monk1, m.attributes, 10))
+print()
 
 
-print("\nAssignment 5.1 b) - Split subset of monk1 into subsets according to selected attribute {}\n".format("A1"))
-monk1_A1 = list()
-monk1_A2346 = list()
-for i in range(len(m.attributes)):
-    if i == 0:
-        monk1_A1.extend(select(monk1_A12346, m.attributes[i], True))
-    elif i != 4:
-        monk1_A2346.extend(select(monk1_A12346, m.attributes[i], True))
-
-# print(len(ºmonk1_A1))
-# print(len(monk1_A2346))
-
-# d3 = get_avg_gain_dict_exclude(monk1_A1)
-d4 = get_avg_gain_dict_exclude(monk1_A2346, exclude=[0, 4])
-print("Subset of monk1 with A1 == True excluded")
-for key, value in d4.items():
-    print(key, "   : ", value)
-
-print("\nAssignment 5.2 - Draw the decision tree up to the first two levels\n")
-most_common_A5 = mostCommon(monk1_A5)
-most_common_A12346 = mostCommon(monk1_A12346)
-most_common_A1 = mostCommon(monk1_A1)
-most_common_A2346 = mostCommon(monk1_A2346)
-print(most_common_A5)
-print(most_common_A12346)
-print(most_common_A1)
-print(most_common_A2346)
-
-print(buildTree(m.monk1, m.attributes, 3))
-drawTree(buildTree(m.monk1, m.attributes, 1))
+# Build the full decision trees for all three Monk datasets using buildTree.
+# Then, use the function check to measure the performance of the decision tree
+# on both the training and test datasets.
+#       For example, to build a tree for monk1 
 
 ################################################################################
 
