8d7e12817ee91465170032d8374fcbbe0104f49b
Henrik Holm <henholm@kth.se>
2019-09-11 11:29:27 +0200

    Added function in main.py

--- python/main.py
+++ python/main.py
@@ -60,23 +60,28 @@ print("monk3: ", entropy_monk3)
 # Based on the results, which attribute should be used for splitting the
 # examples at the root node?
 
+
+def get_avg_gain_dict(dataset):
+    avg_gain_dict = dict()
+    for i in range(len(m.attributes)):
+        attribute_key = "A" + str(i+1)
+        attribute = m.attributes[i]
+        avg_gain = averageGain(dataset, attribute)
+        avg_gain_dict[attribute_key] = avg_gain
+    return avg_gain_dict
+
+
 datasets_train = [m.monk1, m.monk2, m.monk3]
-num_of_attributes = len(m.attributes)
 avg_gain_dict = dict()
 for i, ds in enumerate(datasets_train):
     ds_key = "monk" + str(i+1)
-    avg_gain_sub_dict = dict()
-    for j in range(num_of_attributes):
-        attribute_key = "A" + str(j+1)
-        attribute = m.attributes[j]
-        avg_gain_sub_dict[attribute_key] = averageGain(ds, attribute)
+    avg_gain_sub_dict = get_avg_gain_dict(ds)
     avg_gain_dict[ds_key] = avg_gain_sub_dict
+
 
 print("\n\nAssignment 3 - Where do we find the highest average gain?\n")
 
-# [print(max(x.values()), '\n') for x in avg_gain_dict.values()]
-# [print(*x, sep='\n') for x in [list(avg_gain_dict[y].values()) for y in avg_gain_dict.keys()]]
-print(avg_gain_dict)
+# print(avg_gain_dict)
 [print(name, ": ", max(monk_dict.items(), key=lambda item:item[1])) for name, monk_dict in avg_gain_dict.items()]
 print()
 
@@ -86,12 +91,13 @@ print()
 #           0.01727717693791797
 #           A2 has the highest average gain in the monk3 dataset (A5 close 2nd).
 #           0.29373617350838865
+#           But on average, A5 yields the highest information gain.
 
 #                       Information Gain
 #   Dataset     a1      a2      a3      a4      a5      a6
-#   MONK-1    0.0753  0.0058  0.0047 0.0263   0.2870  0.0007
-#   MONK-2
-#   MONK-3
+#   MONK-1    0.0753  0.0058  0.0047  0.0263  0.2870  0.0007
+#   MONK-2    0.0037  0.0025  0.0010  0.0157  0.0172  0.0062
+#   MONK-3    0.0071  0.2937  0.0008  0.0029  0.2559  0.0071
 ################################################################################
 
 
@@ -121,9 +127,40 @@ print()
 
 
 ################################# Assignment 4 #################################
-def select(dataset, attribute, value):
-    "Return subset of data samples where the attribute has the given value"
-    return [x for x in dataset if x.attribute[attribute] == value]
+# Split the monk1 data into subsets according to the selected attribute using
+# the function select (again, defined in dtree.py) and compute the information
+# gains for the nodes on the next level of the tree. Which attributes should be
+# tested for these nodes?
+#       For the monk1 data draw the decision tree up to the first two levels
+# and assign the majority class of the subsets that resulted from the two splits
+# to the leaf nodes. You can use the predefined function mostCommon
+# (in dtree.py) to obtain the majority class for a dataset.
+#       Now compare your results with that of a predefined routine for ID3. Use
+# the function buildTree(data, m.attributes) to build the decision tree. If you
+# pass a third, optional, parameter to buildTree, you can limit the depth of the
+# generated tree.
+
+monk1_A5 = list()
+monk1_A12346 = list()
+for i in range(len(m.attributes)):
+    if i == 4:
+        monk1_A5.extend(select(m.monk1, m.attributes[i], True))
+    else:
+        monk1_A12346.extend(select(m.monk1, m.attributes[i], True))
+
+# print(len(monk1_A5))
+# print(len(monk1_A12346))
+
+avg_gain_dict = dict()
+for i, ds in enumerate(datasets_train):
+    pass
+#     ds_key = "monk" + str(i+1)
+#     avg_gain_sub_dict = dict()
+#     for j in range(num_of_attributes):
+#         attribute_key = "A" + str(j+1)
+#         attribute = m.attributes[j]
+#         avg_gain_sub_dict[attribute_key] = averageGain(ds, attribute)
+#     avg_gain_dict[ds_key] = avg_gain_sub_dict
 
 ################################################################################
 
