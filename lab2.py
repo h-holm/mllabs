a27d9defd86e5184e280a470374dbe4319cf1965
Henrik Holm <henholm@kth.se>
2019-10-04 23:56:22 +0200

    Solved first part

--- lab2.py
+++ lab2.py
@@ -55,29 +55,42 @@ def instantiate_P(inputs, targets, kernel):
 # ================================================== #
 #            Implementation of equation 4
 # ================================================== #
-def objective(alpha, INPUT_TO_OBJ):
-    np.dot(P)- np.sum(alpha)
-    np.dot()
-    np.sum()
-    return
+# Takes vector alpha as argument.
+# Returns a scalar value, effectively implementing equation 4.
+def objective(alpha):
+    # np.dot()
+    scalar = 0
+    for i, alpha_i in enumerate(alpha):
+        P_i = P[i]
+        for j, alpha_j in enumerate(alpha):
+            P_i_j = P_i[j]
+            scalar += alpha_i * alpha_j * P_i_j
 
+    scalar = 0.5 * scalar - np.sum(alpha)
 
-# 0 <= alphai <= C for all i     AND    SUMi(alphai * ti) = 0
-def zerofun():
-    np.dot()
-    return
+    return scalar
 
 
 # ================================================== #
-#                  Indicator function
+#    Implementation of equality constraint of (10)
+# ================================================== #
+def zerofun(alpha):
+    # if all(elem >= 0 for elem in alpha)
+    scalar = np.dot(alpha, targets)
+
+    return scalar
+
+
+# ================================================== #
+#           Indicator function (equation 6)
 # ================================================== #
 # Implement the indicator function (equation 6) which uses the non-zero αi’s
 # together with their xi’s and ti’s to classify new points.
-def indicator(svs, xs, ys, kernel):
+def indicator(x, y):
     ind = 0.0
-
-    for i in range(len(svs)):
-        ind += (svs[i])[0] * (svs[i])[3] * kernel([x, y], [(svs[i])[1], (svs[i])[2]])
+    for s_v in support_vectors:
+        # Missing b?
+        ind += s_v[0] * s_v[3] * kernel([x, y], [s_v[1], s_v[2]])
 
     return ind
 
@@ -85,7 +98,7 @@ def indicator(svs, xs, ys, kernel):
 # ================================================== #
 #               Plot input data in 2D
 # ================================================== #
-def plot_input_data(class_A, class_B):
+def plot_data_and_dec_boundary(class_A, class_B):
     plt.plot([p[0] for p in class_A],
              [p[1] for p in class_A],
              'b.')
@@ -93,15 +106,47 @@ def plot_input_data(class_A, class_B):
              [p[1] for p in class_B],
              'r.')
 
-    plt.axis('equal')           # Force same scale on both axes.
-    plt.savefig('svmplot.pdf')  # Save a copy in a file.
-    plt.show()                  # Show the plot on the screen.
+    xgrid = np.linspace(-5, 5)
+    ygrid = np.linspace(-4, 4)
+
+    grid = np.array([[indicator(x, y)
+                      for x in xgrid]
+                     for y in ygrid])
+
+    plt.contour(xgrid, ygrid, grid,
+             (-1.0, 0.0, 1.0),
+             colors = ('red', 'black', 'blue'),
+             linewidths = (1, 3, 1))
+
+    plt.axis('equal')               # Force same scale on both axes.
+    plt.savefig('svm_plot.pdf')     # Save a copy in a file.
+    plt.show()                      # Show the plot on the screen.
 
     return
 
 
-# You can use global variables for other things that the function needs
-# (t and K values).
+np.random.seed(100)
+
+kernel = linear_kernel
+# kernel = polynomial_kernel
+
+class_A, class_B = generate_n_dist_dataset()
+# class_A, class_B = generate_other_dataset()
+
+inputs = np.concatenate((class_A, class_B))
+targets = np.concatenate(
+                         (np.ones(class_A.shape[0]),
+                         -np.ones(class_B.shape[0])))
+
+# Number of rows (samples).
+N = inputs.shape[0]
+
+# Randomly reorder the samples.
+permute = list(range(N))
+random.shuffle(permute)
+inputs = inputs[permute, :]
+targets = targets[permute]
+
 N = inputs.shape[0]
 P = np.zeros((N, N))
 for i in range(N):
@@ -118,55 +163,22 @@ for i in range(N):
         # print(kappa)
         P[i][j] = ti*tj*kappa
 
+start = np.zeros(N)
+B = [(0, None) for b in range(N)]
+XC = {'type': 'eq', 'fun': zerofun}
 
-def main():
-    np.random.seed(100)
+ret = minimize(objective, start, bounds=B, constraints=XC)
+alpha = ret['x']
 
-    kernel = linear_kernel
-    # kernel = poly_kernel
+support_vectors = []
+# Extract only non-zero alpha values.
+for i in range(len(alpha)):
+    # Set threshold for filtering out zero-valued alpha values.
+    if alpha[i] > (10**-5):
+        support_vectors.append((alpha[i], inputs[i][0], inputs[i][1], targets[i]))
 
-    class_A, class_B = generate_n_dist_dataset()
-    # class_A, class_B = generate_noisy_dataset()
-
-    inputs = np.concatenate((class_A, class_B))
-    targets = np.concatenate(
-                             (np.ones(class_A.shape[0]),
-                             -np.ones(class_B.shape[0])))
-
-    # Number of rows (samples).
-    N = inputs.shape[0]
-
-    # Randomly reorder the samples.
-    permute = list(range(N))
-    random.shuffle(permute)
-    inputs = inputs[permute, :]
-    targets = targets[permute]
-
-    # P = instantiate_P(inputs=inputs, targets=targets, kernel=linear_kernel)
-
-    print(P)
-
-    start = np.zeros((N, N))
-    B = [(0, None) for b in range(N)]
-    XC = {'type': 'eq', 'fun': zerofun}
-
-    ret = minimize(objective, start, bounds=B, constraints=XC, INPUT_TO_OBJ=P)
-
-    plot_input_data(class_A, class_B)
-
-    # plot_decision_boundary()
-    # xgrid = np.linspace(-5, 5)
-    # ygrid = np.linspace(-4, 4)
-    #
-    # grid = np.array([[indicator(x, y)
-    #                   for x in xgrid]
-    #                  for y in ygrid])
-    #
-    # plt.contour(xgrid, ygrid, grid,
-    #             (-1.0, 0.0, 1.0),
-    #             colors = ('red', 'black', 'blue'),
-    #             linewidths = (1, 3, 1))
+plot_data_and_dec_boundary(class_A, class_B)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
